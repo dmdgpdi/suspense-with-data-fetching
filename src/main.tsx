e92ae6f931728f5d38a34ca36deb6c6ad15121e0
dmdgpdi <dmdgpdi@naver.com>
2024-08-17 19:17:44 +0900

    feat: 간단한 비동기와 Suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,44 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+
+let cache = null; // 간단한 캐싱을 위한 변수
+
+function asynchronousFunction() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Promise over!');
+      resolve(3);
+    }, 500);
+  })
+    .then((value = '') => {
+      console.log(`Promise then ${value}`);
+      return 3;
+    })
+    .catch((error) => {
+      console.warn(`error:`, error);
+    });
+}
+
+function useGetAsyncFunc() {
+  if (cache) {
+    return cache;
+  }
+
+  throw asynchronousFunction().then((data) => {
+    cache = data; // 데이터를 캐싱해두기
+  });
+}
+
+function Page() {
+  const asyncData = useGetAsyncFunc();
+
+  return <h1>Page {asyncData}</h1>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Suspense fallback={<h1>Loading App.tsx</h1>}>
-      <App />
+      <Page />
     </Suspense>
   </React.StrictMode>,
 );
